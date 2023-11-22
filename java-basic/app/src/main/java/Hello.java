8d7b7344c47729d690da35aa99a96fb63cf8f536
kwanmo7 <kwanmo7@naver.com>
2023-11-22 17:47:52 +0900

    .

--- java-basic/app/src/main/java/Hello.java
+++ java-basic/app/src/main/java/Hello.java
@@ -3,9 +3,7 @@
  */
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
-    System.out.println("안녕하세요");
-    System.out.println("안녕하세요2222");
+    System.out.println("Hello, World!");
   }
 
 }
