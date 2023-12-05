704ef8c26d9b15d4352bdb1c4fac115aa2858bab
Lee Kwanmo <kwanmo7@naver.com>
2023-12-06 01:42:31 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam11/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam11/Main.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    // 첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 10(12제곱))이 공백을 사이에 두고 주어진다.
+    // 조건에 주의
     Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    a += scanner.nextInt();
-    a += scanner.nextInt();
+    long a = scanner.nextLong();
+    a += scanner.nextLong();
+    a += scanner.nextLong();
     System.out.print(a);
     scanner.close();
   }
