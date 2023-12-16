61787f4f00b33ad623b4441ea3d2cec6679ad6f4
Lee Kwanmo <kwanmo7@naver.com>
2023-12-16 20:41:27 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam05/Main.java
@@ -6,10 +6,47 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     String input = scanner.nextLine();
-
+    int[] arr = new int[26];
+    int index = 0;
     for (int i = 0; i < input.length(); i++) {
-
+      index = chk(input.charAt(i));
+      arr[index]++;
     }
+    int rlt = getResult(arr);
+    if (rlt == 100) {
+      System.out.print("?");
+    } else {
+      System.out.printf("%s", (char) (rlt + 65));
+    }
+    scanner.close();
   }
 
+  static int chk(char input) {
+    int uni1 = 65;
+    int uni2 = 97;
+
+    for (int i = 0; i <= 25; i++) {
+      if (uni1 + i == input) {
+        return ((uni1 + i) - 65);
+      }
+      if (uni2 + i == input) {
+        return ((uni2 + i) - 97);
+      }
+    }
+    return 0;
+  }
+
+  static int getResult(int[] arr) {
+    int temp = arr[0];
+    int rlt = 0;
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > temp) {
+        temp = arr[i];
+        rlt = i;
+      } else if (arr[i] == temp && temp != 0) {
+        rlt = 100;
+      }
+    }
+    return rlt;
+  }
 }
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam06/Main.java
@@ -0,0 +1,12 @@
+package algorithm.test.baekjoon.level06.exam06;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+
+
+  }
+}
