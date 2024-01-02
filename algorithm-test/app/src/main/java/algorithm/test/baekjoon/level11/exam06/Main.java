1bcf42e7c304037ca28e746a5460a157bed440d3
kwanmo7 <kwanmo7@naver.com>
2024-01-02 18:26:09 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/exam06/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/exam06/Main.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    long sum = 0;
-    for (int i = 1; i <= a - 2; i++) {
-      for (int j = i + 1; j <= a - 1; j++) {
-        for (int k = j + 1; k <= a; k++) {
-          sum++;
-        }
-      }
-    }
-    System.out.println(sum);
+    long n = scanner.nextInt();
+    long rlt = (n - 2) * (n - 1) * n / 6;
+    System.out.println(rlt);
     System.out.println(3);
     scanner.close();
   }
 }
+/*
+ * MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n - 2
+        for j <- i + 1 to n - 1
+            for k <- j + 1 to n
+                sum <- sum + A[i] × A[j] × A[k]; # 코드1
+    return sum;
+}
+ * n : 최대 몇개까지의 숫자인지(입력값)
+ * r : 뽑는 숫자의 갯수
+ * nCr = n! / (n-r)!*r!
+ * 위의 식으로 풀이
+ */
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level12/exam01/Main.java
@@ -0,0 +1,24 @@
+package algorithm.test.baekjoon.level12.exam01;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int input = scanner.nextInt();
+    int reach = scanner.nextInt();
+
+    int[] arr = new int[input];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = scanner.nextInt();
+    }
+
+    while (true) {
+
+
+      break;
+    }
+
+    scanner.close();
+  }
+}
