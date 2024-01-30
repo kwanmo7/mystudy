7010f54804682cebd0ae05dc56676a9c2a33acad
Lee Kwanmo <kwanmo7@naver.com>
2024-01-31 01:12:05 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/exam03/Main.java
@@ -5,7 +5,33 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    StringBuilder sb = new StringBuilder();
     int n = Integer.parseInt(scanner.nextLine());
-
+    for (int i = 0; i < n; i++) {
+      String str = scanner.nextLine();
+      int chk = 0;
+      if (str.charAt(0) == ')' || str.charAt(str.length() - 1) == '(') {
+        sb.append("NO\n");
+        continue;
+      } else {
+        for (int j = 0; j < str.length(); j++) {
+          if (str.charAt(j) == '(') {
+            chk++;
+          } else {
+            chk--;
+          }
+          if (chk < 0) {
+            break;
+          }
+        }
+        if (chk == 0) {
+          sb.append("YES\n");
+        } else {
+          sb.append("NO\n");
+        }
+      }
+    }
+    System.out.print(sb);
+    scanner.close();
   }
 }
